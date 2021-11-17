a203666f6e38ad3ae10ce6f06aecd4d63ccb7bde
StephD48 <stephlynnd37@gmail.com>
2021-11-17 15:00:32 -0800

    Added new folder to repository

---
+++ Basic JavaScript Projects/Project_1expressions_alert/JS/main.js
@@ -0,0 +1,58 @@
+window.alert("My Name is Stephanie") ; // Creating a window alert method
+
+
+document.write('Hello, world!') ; //writing text in my html document
+document.write("<br>"); // Separation the code from the others
+
+
+var A = "This is a string" ; // Defining a variable for A
+document.write(A); // printing the A variable in html document
+document.write("<br>");// Separation of the code from the others
+
+var str = "Fall"; //Defining a variable and giving it a string value
+window.alert(str);// Creating a window alert metod for the string variable
+document.write("<br>");// Separation of the code from the others
+
+
+document.write("Johnny told his brother,\"Stop that! Or I'll tell mom!\"<br>\"I don't care!\"He responded.");// adding escaping to the string quote
+document.write("<br>");//Separation of the code from the others
+
+document.write("Sing like no one\'s listening, Love like you\'ve never been hurt,"
++"Dance like nobody\'s watching, and live like it\'s heaven on earth.\""
++"-Various Sources.");// Concatenating a String
+document.write("<br>");//Separation of code from the others
+
+
+var G = "I haven\'t been on a vacation in a long time. " + "I would love to see New Zealand!" ;//Assigned string values to two variables and concatenated them together
+document.write(G);// assigning a concatenating string to a variable to display in the browser
+document.write("<br>"); //Separation of code from the others
+
+var Students = "Classroom 2", Student_1 = "John", Student_2 = "Chloe", Student_3 = "Stephanie", Student_4 = "Scott";// Assigning multiple variables
+document.write(Student_2);//display one variable 
+document.write("<br>");//Separation of code from the others
+
+
+var Student_1 = "I Like the color red!";//Assiging  a variable
+var Student_1 = Student_1.fontcolor("red") ; //changing the font color of tne variable
+document.write(Student_1); // displaying the font color of the variable
+document.write("<br>");//Separartion of code from the others
+
+
+var Student_2 = "I like the color orange!";//Assiging  a variable
+var Student_2 = Student_2.fontcolor("orange");//changing the font color of tne variable
+document.write(Student_2);// displaying the font color of the variable
+document.write("<br>");//Separartion of code from the others
+
+var Student_3 = "I like the color green!";//Assiging  a variable
+var Student_3 = Student_3.fontcolor("green");//changing the font color of tne variable
+document.write(Student_3);// displaying the font color of the variable
+document.write("<br>");//Separartion of code from the others
+
+var Student_4 = "I like the color purple!";//Assiging  a variable
+var Student_4 = Student_4.fontcolor("purple");//changing the font color of tne variable
+document.write(Student_4);// displaying the font color of the variable
+document.write("<br>");//Separartion of code from the others
+
+document.write(12 * 4); // a statement that is containing an expression
+
+
