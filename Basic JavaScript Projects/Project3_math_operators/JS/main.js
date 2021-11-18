bd295bef3ea58ea949ed3bbdfa89ebe790e153a0
StephD48 <stephlynnd37@gmail.com>
2021-11-18 15:23:38 -0800

    Added new folder to repository

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,80 @@
+function addition_Function() { //Defining a function for addition operation
+    var addition = 4 + 8; //defining a variable for the operation
+    document.getElementById("Math").innerHTML= " 4 + 8 = " + addition;//How the result will be displayed 
+}
+
+
+function subtraction_Function() { //Defining a funtion for subtraction operation
+    var subtraction = 8 - 2;//defining a variable for the operation
+    document.getElementById("subtraction").innerHTML = " 8 - 2 = " + subtraction;//How the result will be displayed 
+}
+
+
+
+function multiplication() {  // Defining a function for multiplication operation
+    var simple_math = 7 * 14 ;//Defining a variable for the multiplication operation
+    document.getElementById("multiplication").innerHTML = " 7 * 14 = " + simple_math;//How the result will be displayed 
+}
+
+
+function division() {//Defining a function for a division operaton
+    var simple_math = 35 / 2 ;//Defining a variable for the division operation
+    document.getElementById("division").innerHTML = " 35 / 2 = " + simple_math; //How the result will be displayed
+}
+
+function more_Math() {//Defining a function for  multiple operations
+    var simple_math = ( 4 + 8) * 12 / 2 - 9;//Defining a variable for the multiple operation
+    document.getElementById("multiple operations").innerHTML = " 4 plus 8, multiplied by 12, divided in half and then subtracted by 9 equals " + simple_math; //How the result will be displayed
+}
+
+function modulus_Operator() { //Defining a function for a modulus operation
+    var simple_math = 33 % 7 ;//Defining a variable for the modulus operation
+    document.getElementById("modulus").innerHTML = "When you divide 33 by 7 you have a remainder of: " + simple_math;//How the result will be displayed
+
+}
+
+function negation_Operator() { //Defining a  function for a negation operation
+    var x = 10;//Defining a variable for the negation operation
+    document.getElementById("negation").innerHTML = -x;//How the result will be displayed
+}
+
+document.write("<br>");//separation of codes
+
+var X = 10;//Defining a variable for an increment operation
+X++;// what the variable will do in the operation
+document.write(X);// How the result will be shown in the document
+
+document.write("<br>");//Separation of codes
+
+var B = 4.25; //Defining a variable for a decrement operation
+B--;//what the variable will do in the operation
+document.write(B);//How the result will be shown in the document
+
+document.write("<br>");//Separation of codes
+
+window.alert(Math.random());//Window alert for a Math.random operation
+
+document.write("<br>");//Separation of codes
+
+window.alert(Math.random() * 50); //Window alert for a Math.random operation
+
+function increment_operator() {//Defining a function for an increment operator
+    var Z= 14;//Defining a variable for the increment operator
+    Z++;//what the variable will do in the operation
+    document.getElementById("increment").innerHTML = Z++;//How the result will be shown in document
+}
+
+function decrement_operator() {//Defining a function for a decrement operator
+    var S = 7.25;//Defining the variable in the decrement operator
+    S--; //What the variable will do in the operation
+    document.getElementById("decrement").innerHTML = S--; // The result of the operation in the document
+
+
+}
+
+
+
+
+
+
+
