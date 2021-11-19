d45dd3b2f197bf5e288ed1fadc480c50746e84d2
StephD48 <stephlynnd37@gmail.com>
2021-11-19 11:49:43 -0800

    Added new folder to repository

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,119 @@
+document.write(typeof 4) //Defining type of operator
+
+document.write("<br>") ;//Separation of codes
+
+document.write(typeof"Dog") ;//Defining type of operator
+
+document.write("<br>"); //Separation of codes
+
+document.write("12" + 7);// Type coercion
+
+document.write("<br>"); //Separation of codes
+
+function my_Function() { //Defining a Function NaN
+    document.getElementById("Test").innerHTML = isNaN('007');//The result will be shown in the display will be false
+}
+
+document.write("<br>");//Separation of codes
+
+function my_Function2() {//Defining a Function NaN
+    document.getElementById("Test2").innerHTML = isNaN('This is a string');//The result will be shown in the display as true
+}
+
+document.write("<br>"); //Separation of codes
+
+function my_Function3() {//Defining a function 
+    document.getElementById("Infinity").innerHTML = 2E410;//The result will show infinity
+}
+
+document.write("<br>");//Separation of codes
+
+function my_Function4() { //Defining a function
+    document.getElementById("-Infinity").innerHTML = -3E502; //The result will show -infinity
+}
+
+document.write("<br>"); //Separation of codes
+
+document.write(325<675);//Boolean statement (true)
+
+document.write("<br>"); //Separation of codes
+
+document.write(33>675);//Boolean statement (false)
+
+document.write("<br>"); //Separation of codes
+
+document.write(7 > 14 && 15 > 8);//Boolean "AND" statement
+
+document.write("<br>"); //Separation of codes
+
+document.write( 8 < 16 || 20 > 5);//Boolean "OR" statement
+
+document.write("<br>"); //Separation of codes
+
+console.log (20 + 35); // Console log method
+
+document.write("<br>"); //Separation of codes
+
+console.log( 10 > 20);//Console log Boolean "false" statement
+
+document.write("<br>"); //Separation of codes
+
+document.write( 14==14); // Double equal "true" statement
+
+document.write("<br>"); //Separation of codes
+
+document.write( 25==37); //Double equal "false" statement
+
+document.write("<br>"); //Separation of codes
+
+A = "time";//Assigning a Variable
+B = "time";//Assigning a variable
+document.write(A===B);//Triple equal sign true statement
+
+document.write("<br>"); //Separation of codes
+
+X = 82;// Assigning a Variable
+Y = "83";//Assigning a variable
+document.write(X === Y);//Triple equal sign false statement
+
+document.write("<br>"); //Separation of codes
+
+D = "75";//Assigning a Variable
+G = 75;//Assiging a Variable
+document.write(D === G);//Triple equal sign false statement
+
+document.write("<br>"); //Separation of codes
+
+F = 300 ;//Assigning a Variable
+J = 100 ;//Assigning a variable
+document.write(F === J);//Triple equal sign false statement
+
+document.write("<br>"); //Separation of codes
+
+function not_Function() { // Defining a function
+    document.getElementById("Not").innerHTML = !(30 > 15);//Result of NOT Operator False
+
+}
+
+document.write("<br>"); //Separation of codes
+
+function not_Function2() {//Defining a function
+    document.getElementById("Not-2").innerHTML = !(8 > 48); // Result of NOT Operator True
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
