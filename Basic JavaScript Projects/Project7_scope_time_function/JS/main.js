26f9cefdabd49608678a042bc258939272c28f7c
StephD48 <stephlynnd37@gmail.com>
2021-11-19 18:15:18 -0800

    added new code to file

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -54,4 +54,20 @@ function Over_Age_Function()  {//Defining a function
         
     }
     document.getElementById("How_old_are_you?").innerHTML = Drink;//Result
+
+}
+
+function Time_function() {
+    var Time = new Date().getHours() ;
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";       
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
